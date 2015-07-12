bc68cfc2419cb1412831a571a2bf8b526c0a3e9f
Oscar Lodriguez <o.lodriguez@gmail.com>
2015-07-12 21:06:00 +0200

    moved data from state to normal variable since its a static app

--- app/src/components/search-bar.jsx
+++ app/src/components/search-bar.jsx
@@ -4,7 +4,6 @@ class SearchBar extends React.Component {
 
   constructor() {
     super();
-
   }
 
   render () {
--- app/src/filterable-product-table.jsx
+++ app/src/filterable-product-table.jsx
@@ -6,23 +6,35 @@ class FilterableProductTable extends React.Component {
 
   constructor() {
     super();
-    this.state = {
-      products: [
-        {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-        {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-        {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-        {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-        {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-        {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-      ]
-    };
+
+    // Turn on when you are using interactive data. For static data just use a variable
+    // this.state = {
+    //  products: [
+    //    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    //    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    //    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    //    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    //    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    //    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+    //  ]
+    //};
   }
 
   render() {
+
+    var products = [
+      {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+      {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+      {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+      {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+      {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+      {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+    ];
+
     return (
       <div>
         <SearchBar />
-        <ProductTable products={this.state.products} />
+        <ProductTable products={products} />
       </div>
     )
   }
