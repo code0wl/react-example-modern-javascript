9fa146de7237a95799880de2d7bd3e6a2f2cece9
oscarlodriguez <o.lodriguez@gmail.com>
2016-03-04 10:57:58 +0100

    semantically more correct with html tags

--- app/main.jsx
+++ app/main.jsx
@@ -7,7 +7,7 @@ import FilterableProductTable from './src/filterable-product-table';
 renderShell();
 
 function renderShell() {
-    var shell = document.createElement('div');
+    var shell = document.createElement('main');
     shell.className = 'app-shell';
     document.body.appendChild(shell);
     render(<FilterableProductTable/>, shell);
