ed64ae4c4f80005ba18536903aac7cbba8a26ddb
oscarlodriguez <o.lodriguez@gmail.com>
2016-04-17 09:47:42 +0200

    seperated model out of main

--- app/src/filterable-product-table.jsx
+++ app/src/filterable-product-table.jsx
@@ -1,53 +1,44 @@
 import React from 'react';
 import ProductTable from './components/product-table';
 import SearchBar from './components/search-bar';
+import products from './models/products';
 
 class FilterableProductTable extends React.Component {
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.handleUserInput = this.handleUserInput.bind(this);
+        this.handleUserInput = this.handleUserInput.bind(this);
 
-    this.state = {
-      filterText: '',
-      inStockOnly: false
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        }
     }
-  }
 
-  handleUserInput(filterText, inStockOnly) {
-    this.setState({
-      filterText: filterText,
-      inStockOnly: inStockOnly
-    });
-  }
+    handleUserInput(filterText, inStockOnly) {
+        this.setState({
+            filterText: filterText,
+            inStockOnly: inStockOnly
+        });
+    }
 
-  render() {
-
-    const products = [
-      {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-      {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-      {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-      {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-      {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-      {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-    ];
-
-    return (
-      <div>
-        <SearchBar
-          filterText={this.state.filterText}
-          inStockOnly={this.state.inStockOnly}
-          onUserInput={this.handleUserInput}
-        />
-        <ProductTable
-          products={products}
-          filterText={this.state.filterText}
-          inStockOnly={this.state.inStockOnly}
-        />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    onUserInput={this.handleUserInput}
+                />
+                <ProductTable
+                    products={products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+            </div>
+        )
+    }
 
 }
 
